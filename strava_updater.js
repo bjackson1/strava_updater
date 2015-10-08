a59bac663e4d769b192d9335ade1b77a3f30e6c8
Brett <Brett.Jackson@hscic.gov.uk>
2015-10-08 21:26:15 +0100

    First ever go at node.js...

---
+++ strava_updater.js
@@ -0,0 +1,62 @@
+//var http = require('http')
+var https = require('https')
+var fs = require('fs');
+var process = require('process')
+
+var accessTokenFile = process.env.HOME + '/strava_access_token'
+var startTime = (((new Date).getTime() / 1000) - 180000000).toFixed(0);
+
+fs.readFile(accessTokenFile, 'utf8', function(err,data){
+    if (err) {
+        return console.log(err);
+    }
+    getActivities(data.trim());});
+
+function getActivities(accessToken, callback)
+{
+    console.log('requesting...');
+    return https.get(
+        {
+			host: 'www.strava.com',
+			path: '/api/v3/athlete/activities?after=' + startTime + '&access_token=' + accessToken + '&per_page=100'
+		}, function(response)
+		{
+			var body = '';
+			response.on('data', function(d)
+				{
+					body += d;
+				});
+			response.on('end', function()
+            {
+					var parsed = JSON.parse(body);
+                    var matchingActivities = [];
+					for(var i = 0; i < parsed.length; i++)
+					{
+                        if (checkActivity(parsed[i]))
+                        {
+                            matchingActivities.push(parsed[i]);
+                        }
+					}
+                    
+                    for(var i = 0; i < matchingActivities.length; i++)
+					{
+                        console.log(matchingActivities[i].id, matchingActivities[i].private, matchingActivities[i].name, matchingActivities[i].distance);
+					}
+				});
+		});
+}
+
+function checkActivity(activity)
+{   
+    if (activity.distance < 30000 && !activity.private)
+    {
+        return true;
+    }
+    
+    return false;
+}
+
+function makeActivityPrivate(activity, accessToken)
+{
+    
+}
